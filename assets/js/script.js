d27bdd782d0cc720fa320a1da850e23bd3cfae2d
Valerie Gleason <valerie.gleason@gmail.com>
2022-06-09 16:36:44 -0500

    Adding 5 day weather forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var searchTerm = document.querySelector("#search-field");
 var cityInfoEl = document.querySelector("#city-info");
 var apiKey = "126f457772cbca5e011de1b69127c8f8"
 var currentDayInfoEl = document.querySelector("#current-day-info")
+var fiveDayForecastEl = document.querySelector("#five-day-forecast")
 
 // get the current date
 var currentDate = luxon.DateTime.now().toFormat('MM/dd/yyyy')
@@ -13,7 +14,7 @@ var formSubmitHandler = function(event) {
     // get value from input element
     var city = searchTerm.value
         // .trim();
-    console.log(city)
+    
     if (city) {
         cityToLatLong(city);
         searchTerm.value = "";
@@ -73,6 +74,7 @@ var searchCity = function(lat, lon) {
         if (response.ok) {
            response.json().then(function(data) {
                displayCurrentDayWeather(data)
+               displayFiveDayForecast(data)
            });
         } else {
             alert("Error: City cannot be found");
@@ -85,10 +87,10 @@ var searchCity = function(lat, lon) {
 };
 
 var displayCurrentDayWeather = function(array) {
-    console.log(array)
+    
     // create element to store current temperature
     var tempEl = document.createElement("p")
-    tempEl.textContent = "Temp: " + array.current.temp
+    tempEl.textContent = "Temp: " + array.current.temp + " °F"
     // create element to store current wind
     var windEl = document.createElement("p")
     windEl.textContent = "Wind: " + array.current.wind_speed + " MPH"
@@ -103,6 +105,54 @@ var displayCurrentDayWeather = function(array) {
 
     // append all to the div
     currentDayInfoEl.append(tempEl,windEl,humidityEl,UVEl)
+}
+
+var displayFiveDayForecast = function(array) {
+
+    // clear out any five day forecast from previous searches
+    fiveDayForecastEl.textContent=''
+    // for loop to get the next 5 days of data
+    for (var i = 1; i < 6; i++) {
+        // convert the unix timestamp to a human readable date using luxon
+        var date  = luxon.DateTime.fromSeconds(array.daily[i].dt).toFormat('MM/dd/yyyy')
+
+        // create the card to hold each day
+        var fiveDayDiv = document.createElement("div")
+        fiveDayDiv.classList.add("five-day-forecast-card")
+
+        // save the date as a heading
+        var fiveDayDate = document.createElement("h3")
+        fiveDayDate.textContent = date;
+
+        // get the icon http://openweathermap.org/img/w/10d.png
+        var fiveDayIconUrl = "http://openweathermap.org/img/w/" + array.daily[i].weather[0].icon + ".png"
+        var fiveDayIconImg = document.createElement("img")
+        fiveDayIconImg.setAttribute("src",fiveDayIconUrl)
+        fiveDayIconImg.setAttribute("alt", "Glyph depicting the weather for the day")
+
+        var fiveDayIcon = document.createElement("p")
+
+        fiveDayIcon.append(fiveDayIconImg)
+        // fiveDayIcon.innerHTML = array.daily[i].weather.icon
+        console.log(fiveDayIconImg)
+        // get the temp for the day
+        var fiveDayTemp = document.createElement("p")
+        fiveDayTemp.textContent= "Temp: " + array.daily[i].temp.day + " °F"
+
+        // get the wind for the day
+        var fiveDayWind = document.createElement("p")
+        fiveDayWind.textContent= "Wind: " + array.daily[i].wind_speed + " MPH"
+
+        // get the humidity for the day
+        var fiveDayHumidity = document.createElement("p")
+        fiveDayHumidity.textContent= "Humidity: " + array.daily[i].humidity + " %"
+
+        fiveDayDiv.append(fiveDayDate, fiveDayIcon, fiveDayTemp, fiveDayWind, fiveDayHumidity)
+
+        // append each card to the container
+        fiveDayForecastEl.append(fiveDayDiv)
+    }
+
 }
 
 searchFormEl.addEventListener("submit", formSubmitHandler)
