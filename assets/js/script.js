68f3507f12c0442efcb55820fe1e78ab572fbb8c
Valerie Gleason <valerie.gleason@gmail.com>
2022-06-09 15:24:07 -0500

    Display details for current day

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@ var searchFormEl = document.querySelector("#search-form");
 var searchTerm = document.querySelector("#search-field");
 var cityInfoEl = document.querySelector("#city-info");
 var apiKey = "126f457772cbca5e011de1b69127c8f8"
+var currentDayInfoEl = document.querySelector("#current-day-info")
 
+// get the current date
+var currentDate = luxon.DateTime.now().toFormat('MM/dd/yyyy')
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -12,18 +15,25 @@ var formSubmitHandler = function(event) {
         // .trim();
     console.log(city)
     if (city) {
-        searchCity(city);
+        cityToLatLong(city);
         searchTerm.value = "";
-        console.log(searchFormEl.value)
-        cityInfoEl.textContent = city;
+
+        cityInfoEl.textContent = city + " (" + currentDate + ")";
+
     } else {
         alert("Please enter a city")
     }
 }
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-var searchCity = function(city) {
-    // format the github api url
+
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+var cityToLatLong = function(city) {
+    // format the open weather map api url to pull lat & long
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+    //clear out the previous results
+    currentDayInfoEl.textContent = ''
 
     // make a request to the url
     fetch(apiUrl)
@@ -31,7 +41,7 @@ var searchCity = function(city) {
         // request was successful
         if (response.ok) {
            response.json().then(function(data) {
-               console.log(data)
+               getLatLong(data)
            });
         } else {
             alert("Error: City cannot be found");
@@ -42,5 +52,57 @@ var searchCity = function(city) {
             alert("Unable to weather service");
         });
 };
+
+var getLatLong = function(array) {
+    var lat = array.coord.lat
+    var lon = array.coord.lon
+    console.log(lat)
+    console.log(lon)
+    // use one API to search for the city based on latitude and longitude
+    searchCity(lat,lon)
+}
+
+var searchCity = function(lat, lon) {
+    // format the open weather map api url to pull lat & long
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=" + apiKey + "&units=imperial";
+
+    // make a request to the url
+    fetch(apiUrl)
+        .then(function(response) {
+        // request was successful
+        if (response.ok) {
+           response.json().then(function(data) {
+               displayCurrentDayWeather(data)
+           });
+        } else {
+            alert("Error: City cannot be found");
+        }
+        })
+        .catch(function(error) {
+            // Catch for any errors from the server
+            alert("Unable to weather service");
+        });
+};
+
+var displayCurrentDayWeather = function(array) {
+    console.log(array)
+    // create element to store current temperature
+    var tempEl = document.createElement("p")
+    tempEl.textContent = "Temp: " + array.current.temp
+    // create element to store current wind
+    var windEl = document.createElement("p")
+    windEl.textContent = "Wind: " + array.current.wind_speed + " MPH"
+
+    // create element to store humidity
+    var humidityEl = document.createElement("p")
+    humidityEl.textContent = "Humidity: " + array.current.humidity + " %"
+
+    // create element to store UV
+    var UVEl = document.createElement("p")
+    UVEl.textContent = "UV Index: " + array.current.uvi
+
+    // append all to the div
+    currentDayInfoEl.append(tempEl,windEl,humidityEl,UVEl)
+}
 
 searchFormEl.addEventListener("submit", formSubmitHandler)
